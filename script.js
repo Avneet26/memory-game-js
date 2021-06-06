cfb83a1a2d2f41f3c39589febd84488be7e0d36e
AvneetVirdi <avneetvirdi26@gmail.com>
2021-06-07 01:47:11 +0530

    added timer

--- script.js
+++ script.js
@@ -64,6 +64,10 @@ if (show) {
 strtBtn.addEventListener("click", function () {
     show = true;
     gameRunning = true;
+    strtBtn.innerText = "Playing ..";
+    if (gameRunning) {
+        strtBtn.setAttribute("disabled", true);
+    }
     if (show) {
         for (let i = 0; i < 16; i++) {
             allCells[i].style.backgroundColor = cellObj[i].color;
@@ -80,6 +84,7 @@ strtBtn.addEventListener("click", function () {
                 allCells[i].style.backgroundColor = "gray";
             }
         }
+        startTimer();
     }, 1500);
 });
 
@@ -87,41 +92,64 @@ let clickNo = 0;
 let currColor = "none";
 let firstClickElem;
 function handleCellClick(e) {
-    let currElem = e.currentTarget;
-    let clickedColor = currElem.getAttribute("color");
-    let clickedId = currElem.getAttribute("id");
-    //First click
-    if (clickNo == 0) {
-        //save first clicked elem
-        firstClickElem = currElem;
-        //save first clicked color
-        currColor = clickedColor;
-        //change color on ui
-        currElem.style.backgroundColor = clickedColor;
-        clickNo = 1;
-    }
-
-    //Match on the second color
-    else if (clickNo == 1) {
-        //Check for a match
-        if (clickedColor == currColor) {
-            currElem.style.backgroundColor = clickedColor; //change color on ui
-            let firstId = firstClickElem.getAttribute("id"); //get the id of element we were mathing with
-            cellObj[firstId].found = true; //set found true for both
-            cellObj[clickedId].found = true;
-            console.log("matched");
-            clickNo = 0; //reset click counter
-        } else {
+    if (gameRunning == true) {
+        let currElem = e.currentTarget;
+        let clickedColor = currElem.getAttribute("color");
+        let clickedId = currElem.getAttribute("id");
+        //First click
+        if (clickNo == 0) {
+            //save first clicked elem
+            firstClickElem = currElem;
+            //save first clicked color
+            currColor = clickedColor;
+            //change color on ui
             currElem.style.backgroundColor = clickedColor;
-            setTimeout(function () {
-                firstClickElem.style.backgroundColor = "gray";
-                currElem.style.backgroundColor = "gray";
-            }, 500);
-            console.log("not a match");
-            currColor = "none";
-            clickNo = 0;
+            clickNo = 1;
+        }
+
+        //Match on the second color
+        else if (clickNo == 1) {
+            //Check for a match
+            if (clickedColor == currColor) {
+                currElem.style.backgroundColor = clickedColor; //change color on ui
+                let firstId = firstClickElem.getAttribute("id"); //get the id of element we were mathing with
+                cellObj[firstId].found = true; //set found true for both
+                cellObj[clickedId].found = true;
+                console.log("matched");
+                clickNo = 0; //reset click counter
+            } else {
+                currElem.style.backgroundColor = clickedColor;
+                setTimeout(function () {
+                    firstClickElem.style.backgroundColor = "gray";
+                    currElem.style.backgroundColor = "gray";
+                }, 500);
+                console.log("not a match");
+                currColor = "none";
+                clickNo = 0;
+            }
+        }
+
+        //after every click check if game is over
+        if (isGameOver()) {
+            console.log("gameOver");
+            strtBtn.removeAttribute("disabled");
+            strtBtn.innerText = "Well Played";
+            gameRunning = false;
+            stopTimer();
+        }
+    } else {
+        console.log("start the game");
+    }
+}
+
+//if any found value in cellobj is false return false
+//false -> game not over
+//true -> game over
+function isGameOver() {
+    for (let x in cellObj) {
+        if (cellObj[x].found == false) {
+            return false;
         }
     }
-
-    console.log(cellObj);
+    return true;
 }
